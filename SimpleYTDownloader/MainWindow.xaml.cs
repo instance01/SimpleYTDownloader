d61b821dd2bcd4d5dfd49757c3c41fc9cd85436d
InstanceLabs <danielratke97@hotmail.de>
2014-01-27 21:45:55 +0100

    bigger update; adds progressbar to each video

--- SimpleYTDownloader/MainWindow.xaml.cs
+++ SimpleYTDownloader/MainWindow.xaml.cs
@@ -15,13 +15,44 @@ using MahApps.Metro.Controls;
 using MahApps.Metro;
 using YoutubeExtractor;
 using System.Threading;
+using System.ComponentModel;
 
 namespace SimpleYTDownloader
 {
     public partial class MainWindow : MetroWindow
     {
+
+        public IList<Video> Videos { get; set; }
+
+        public class Video : INotifyPropertyChanged
+        {
+            public int percent;
+
+            public string Name { get; set; }
+            public int Progress
+            {
+                get { return this.percent; }
+                set
+                {
+                    percent = value;
+                    NotifyPropertyChanged("Progress");
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void NotifyPropertyChanged(String info)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(info));
+                }
+            }
+        }
+
         public MainWindow()
         {
+            Videos = new List<Video>();
             InitializeComponent();
         }
 
@@ -40,7 +71,7 @@ namespace SimpleYTDownloader
                 string s = (string)e.Data.GetData(DataFormats.Text, false);
                 if (!listbox1.Items.Contains(s))
                 {
-                    listbox1.Items.Add(s);
+                    addItem(s);
                 }
             }
             if (e.Data.GetDataPresent(DataFormats.UnicodeText))
@@ -48,7 +79,7 @@ namespace SimpleYTDownloader
                 string s = (string)e.Data.GetData(DataFormats.UnicodeText, false);
                 if (!listbox1.Items.Contains(s))
                 {
-                    listbox1.Items.Add(s);
+                    addItem(s);
                 }
             }
             if (e.Data.GetDataPresent(DataFormats.SymbolicLink))
@@ -56,7 +87,7 @@ namespace SimpleYTDownloader
                 string s = (string)e.Data.GetData(DataFormats.SymbolicLink, false);
                 if (!listbox1.Items.Contains(s))
                 {
-                    listbox1.Items.Add(s);
+                    addItem(s);
                 }
             }
             if (e.Data.GetDataPresent(DataFormats.Html))
@@ -64,7 +95,7 @@ namespace SimpleYTDownloader
                 string s = (string)e.Data.GetData(DataFormats.Html, false);
                 if (!listbox1.Items.Contains(s))
                 {
-                    listbox1.Items.Add(s);
+                    addItem(s);
                 }
             }
             if (e.Data.GetDataPresent(DataFormats.OemText))
@@ -72,9 +103,20 @@ namespace SimpleYTDownloader
                 string s = (string)e.Data.GetData(DataFormats.OemText, false);
                 if (!listbox1.Items.Contains(s))
                 {
-                    listbox1.Items.Add(s);
+                    addItem(s);
                 }
             }
+        }
+
+        //Dictionary<int, ProgressBar> links_ = new Dictionary<int, ProgressBar>();
+
+        public void addItem(String s)
+        {
+            //listbox1.Items.Add(new { Name=s, Progress=b });
+            Video v = new Video() {Name=s, Progress=0};
+            Videos.Add(v);
+            listbox1.Items.Add(v);
+            //links_.Add(listbox1.Items.Count - 1, b);
         }
 
         private void Button_Click(object sender_, RoutedEventArgs e)
@@ -83,22 +125,37 @@ namespace SimpleYTDownloader
             //progressbar1.Minimum = 0;
             int count = listbox1.Items.Count;
 
-            foreach (string i in listbox1.Items)
+            for (int i = 0; i < Videos.Count; i++)
             {
-                string link = i;
-
                 progressring1.IsActive = true;
                 label1.Visibility = System.Windows.Visibility.Visible;
-
-                Thread t = new Thread(() => downloadVideo(link, count));
+                MessageBox.Show(i.ToString());
+                Video v = null;
+                if (i != -1 && i < Videos.Count)
+                {
+                    v = Videos[i];
+                }
+                Thread t = new Thread(() => downloadVideo(v, count, i));
                 t.Start();
             }
+               /* foreach (Video i in listbox1.Items)
+                {
+                    //string link = i;
+
+                    progressring1.IsActive = true;
+                    label1.Visibility = System.Windows.Visibility.Visible;
+
+                    Thread t = new Thread(() => downloadVideo(i, count, c));
+                    t.Start();
+
+                    c++; // C++ ô.ô
+                }*/
         }
 
 
-        public void downloadVideo(string link, int count)
+        public void downloadVideo(Video link, int count, int c)
         {
-            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link.Name);
 
             VideoInfo video = videoInfos
             .Where(info => info.CanExtractAudio)
@@ -109,16 +166,30 @@ namespace SimpleYTDownloader
 
             //audioDownloader.DownloadProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => progressbar1.Value = args.ProgressPercentage / count));
             //audioDownloader.AudioExtractionProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => progressbar1.Value = args.ProgressPercentage * 0.15));
-            audioDownloader.DownloadProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => label1.Content = Convert.ToString(args.ProgressPercentage / count) + "%"));
-            audioDownloader.AudioExtractionProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => label1.Content = Convert.ToString(args.ProgressPercentage / count) + "%"));
+            audioDownloader.DownloadProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => {
+                Videos[c - 1].Progress = (int)args.ProgressPercentage;
+                label1.Content = Convert.ToString(args.ProgressPercentage) + "%";
+            }));
+            audioDownloader.AudioExtractionProgressChanged += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() => {
+                Videos[c - 1].Progress = (int)args.ProgressPercentage;
+                label1.Content = Convert.ToString(args.ProgressPercentage) + "%";
+            }));
             audioDownloader.DownloadFinished += (sender, args) => this.Dispatcher.BeginInvoke(new Action(() =>
             {
-                label1.Visibility = System.Windows.Visibility.Hidden;
-                label1.Content = "0%";
-                progressring1.IsActive = false;
-                //progressbar1.Value = 0;
-                listbox1.Items.Remove(link);
-
+                if (listbox1.Items.Count < 2)
+                {
+                    label1.Visibility = System.Windows.Visibility.Hidden;
+                    label1.Content = "0%";
+                    progressring1.IsActive = false;
+                    listbox1.Items.Remove(link);
+                    Videos.Remove(link);
+                    Videos.Clear();
+                }
+                else
+                {
+                    listbox1.Items.Remove(link);
+                    Videos.Remove(link);
+                }
             }));
 
 
@@ -132,7 +203,8 @@ namespace SimpleYTDownloader
             {
                 if (Clipboard.ContainsData(DataFormats.Text) || Clipboard.ContainsData(DataFormats.UnicodeText) || Clipboard.ContainsData(DataFormats.OemText))
                 {
-                    listbox1.Items.Add(Clipboard.GetText());
+                    addItem(Clipboard.GetText());
+                    //listbox1.Items.Add(Clipboard.GetText());
                 }
 
                 e.Handled = true;
